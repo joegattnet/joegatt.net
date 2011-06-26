cca1d7b65744558a7d79750d704a902720489f75
Joe Gatt <dev@joegatt.net>
2011-06-26 23:14:10 +0100

    Removed hardcoded date formats.

--- www/_etc/cgi/enface/version.cgi
+++ www/_etc/cgi/enface/version.cgi
@@ -7,7 +7,13 @@ formRead("get");
 my $dbh = connectDB();
 
 $sth = $dbh->prepare(qq{
-    SELECT target_text.text AS target,$date_format_target_text,DATE_FORMAT(target_text.date_created,'%e %b %Y at %H:%i UTC'),target_text.Id,score,users.Id,username 
+    SELECT target_text.text AS target,
+    DATE_FORMAT(target_text.date_created,'%Y-%m-%dT%H:%i:%sZ'),
+    DATE_FORMAT(target_text.date_created,'%e %b %Y at %H:%i UTC'),
+    target_text.Id,
+    score,
+    users.Id,
+    username 
     FROM target_text,users 
     WHERE target_text.p=? AND target_text.version=? AND users.Id=target_text.user_id AND target_text.book_id=? 
     LIMIT 1
@@ -15,14 +21,14 @@ $sth = $dbh->prepare(qq{
 
 $sth->execute($p, $version, $b);
 
-while (my ($target,$date_string,$date,$target_id,$score,$u,$user_name) = $sth->fetchrow_array()) {
+while (my ($target,$date_iso8601,$date_full,$target_id,$score,$u,$user_name) = $sth->fetchrow_array()) {
  	$output .= qq~
     NB.versions['p$target_id'] = {
       userId: $u,
       userName: '$user_name',
       score: $score,
-      dateString: '$date',
-      date: '$date_string',
+      date_iso8601: '$date_iso8601';
+      date_full: '$date_full',
       version: $version,
       isLatest: false
     }
--- www/_etc/javascript/src/versions.-.js
+++ www/_etc/javascript/src/versions.-.js
@@ -1,72 +1,71 @@
 NB.Versions = {
 		display: function(q){
 		NB.Nav.track('Versions.display',q);
-//		    var [user_id,username,score,date_string,date,version,latest] = NB.versions[q];
-        var pq = 'p'+q;
-		    var user_id = NB.versions[pq][0];
-        var username = NB.versions[pq][1];
-        var score = NB.versions[pq][2];
-        var date_string = NB.versions[pq][3];
-        var date = NB.versions[pq][4];
-        var version = NB.versions[pq][5];
-        var latest = NB.versions[pq][6];
-		    var showusername = (NB.User.id==NB.versions[pq][0]?'me':username);
 
-        NB.Nav.track(2,'_trackPageview',NB.crumb.lastloaded==undefined?location.pathname:NB.crumb.lastloaded+(latest?'':'/v/'+q));
+        var pq = 'p'+q;
+        var thisVersion = NB.versions[pq];
+        var showUserName = (NB.User.id===thisVersion.userId?'me':thisVersion.userName);
+
+        NB.Nav.track(2,'_trackPageview',NB.crumb.lastloaded==undefined?location.pathname:NB.crumb.lastloaded+(thisVersion.latest?'':'/v/'+q));
         
-// NB.Nav.track(2,'Enface', 'Version', 'v'+version+(latest?' (latest)':''), q);
-// NEXTREL        $('version_info_panel').update('<p>version '+version+(score>0?' &#x25b2;'+score:'')+'</p><p><a href="'+NB.root+'users.shtml?u='+user_id+'&b='+NB.book.id+'">'+username+'</a>, <span title="'+date+'">'+date_string+'</span></p>');
+        var fullDescription = 'Paragraph ' + NB.p.current + ' version 0.' + thisVersion.version + ', score: ' + (thisVersion.score>0?thisVersion.score:'');
         
-        var fullDescription = 'Paragraph ' + NB.p.current + ' version 0.' + version + ' score ' + (score>0?score:'');
-        
-        $('#version_info_panel').html('<p title="' + fullDescription + '">&para; ' + NB.p.current + ' v<span class="version-chunk-source">0.' + version + '</span>' + (score>0?' &#x25b2;'+score:'')+'</p><p class="user-name user-'+username+'" data-username="'+username+'">'+showusername+', <span title="'+date+'">'+date_string+'</span></p>');
+        $('#version_info_panel').html('<p title="' + fullDescription + '">&para; ' + NB.p.current + ' v<span class="version-chunk-source">0.' + thisVersion.version + '</span>' + (thisVersion.score>0?' &#x25b2;'+thisVersion.score:'')+'</p><p class="user-name user-'+showUserName+'" data-username="'+thisVersion.userName+'">'+showUserName+', <span title="'+thisVersion.date_iso8601+'" class="timeago">'+thisVersion.date_full+'</span></p>');
         
         NB.Nav.crumb.version_chunk();
         
         $('#versions_nav a').each(function(){
           $(this).unbind('click.versions');
         });
-        if (version==1) {
+        if (thisVersion.version==1) {
           $('#version_first').bind('click.versions',NB.Tools.empty);
           $('#version_first').addClass('disabled');
           $('#version_previous').bind('click.versions',NB.Tools.empty);
           $('#version_previous').addClass('disabled');
         } else {
           $('#version_first').bind('click.versions',function(){
-            NB.Versions.get(q,version,1)
+            NB.Versions.get(q,thisVersion.version,0)
           });
           $('#version_first').removeClass('disabled');
           $('#version_previous').bind('click.versions',function(){
-            NB.Versions.get(q,version,-1)
+            NB.Versions.get(q,thisVersion.version,-1)
           });
           $('#version_previous').removeClass('disabled');
         }
-        if (latest) {
+        if (thisVersion.isLatest) {
           $('#version_next').bind('click.versions',NB.Tools.empty);
           $('#version_next').addClass('disabled');
           $('#version_latest').bind('click.versions',NB.Tools.empty);
           $('#version_latest').addClass('disabled');
         } else {
           $('#version_next').bind('click.versions',function(){
-            NB.Versions.get(q,version,2)
+            NB.Versions.get(q,thisVersion.version,2)
           });
           $('#version_next').removeClass('disabled');
           $('#version_latest').bind('click.versions',function(){
-            NB.Versions.get(q,version,1)
+            NB.Versions.get(q,thisVersion.version,1)
           });
           $('#version_latest').removeClass('disabled');
         }
     },
     get:function(q,current_version,direction){
       var p_element = NB.Enface.get_target(NB.p.current);
-      if(direction!=0&&(current_version+direction!=NB.versions['p'+$(NB.Enface.get_target(NB.p.current)).data('id')][5])){
+      var getVersion = current_version+direction;
+      
+      if(direction == 0){
+        getVersion = 1;
+      }else if(direction == 2){
+        //getVersion = NB.versions[pq][6];
+      }
+      
+      if(direction!=0&&(getVersion!=NB.versions['p'+$(NB.Enface.get_target(NB.p.current)).data('id')][5])){
          p_element.addClass('version');
          NB.Editors.remove(p_element);
  			 	 $(p_element).animate({color:NB.S.color.version_loading},'fast');
 			 	 NB.Ajax.html(
             'get',
             '#hidden',
-            NB.root+'_etc/cache/enface--version-b='+NB.book.id+'&p='+NB.p.current+'&version='+(current_version+direction)+'.html',
+            NB.root+'_etc/cache/enface--version-b='+NB.book.id+'&p='+NB.p.current+'&version='+getVersion+'.html',
             '',
             true,
             null,
