a226c751bf177e022bda7f89702970a67cabe45d
Joe Gatt <dev@joegatt.net>
2011-07-17 17:29:49 +0100

    Fixing image margins.

--- www/_etc/javascript/src/ui.image.js
+++ www/_etc/javascript/src/ui.image.js
@@ -48,17 +48,17 @@ NB.Ui.image = {
 /******************************************************************************/
 
 $('.prefix_1 .notes-list img[src*="-wb-3-0-"], .prefix_1 .notes-list img[src*="-fw-220-0-"]').live('click', function (event) {
-  NB.Ui.image.open(event, '220px', '120px', '540px', '300px', 3, 7, 0, '10px', '-80px', '10px');
+  NB.Ui.image.open(event, '220px', '120px', '540px', '300px', 3, 7, 0, 0, '-80px', 0);
   return false;
 });
 
 $('.page-notes-index .notes-list img[src*="-wb-3-0-"], .page-tags-page .notes-list img[src*="-wb-3-0-"], #notes_index .notes-list img[src*="-fw-220-0-"], #tags_page .notes-list img[src*="-fw-220-0-"]').live('click', function (event) {
-  NB.Ui.image.open(event, '220px', '120px', '540px', '300px', 3, 7, 0, '10px', 0, '10px');
+  NB.Ui.image.open(event, '220px', '120px', '540px', '300px', 3, 7, 0, 0, 0, 0);
   return false;
 });
 
-$('.suffix_2 .notes-list img[src*="-wb-4-0-"]').live('click', function (event) {
-  NB.Ui.image.open(event, '300px', '170px', '540px', '300px', 4, 7, 0, '10px', 0, '10px');
+$('.notes-list img[src*="-wb-4-0-"]').live('click', function (event) {
+  NB.Ui.image.open(event, '300px', '170px', '540px', '300px', 4, 7, 0, 0, 0, 0);
   return false;
 });
 
