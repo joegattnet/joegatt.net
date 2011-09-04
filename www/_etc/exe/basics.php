6aa57de5972b6377c79c7ae7d6badb2cbac4c034
Joe Gatt <dev@joegatt.net>
2011-09-04 16:20:56 +0100

    Enables script to run outside of Apache.

--- www/_etc/exe/basics.php
+++ www/_etc/exe/basics.php
@@ -2,29 +2,38 @@
 
   ini_set("include_path", ini_get("include_path") . PATH_SEPARATOR . "../lib/php" . PATH_SEPARATOR . "../src/php" . PATH_SEPARATOR);
 
-  //Not good enough - this will always be unset when not run on Apache (via cron)
-  //(Needs to read directory)
-  //if(isset($_SERVER['SERVER_NAME'])) {
-  //  DEFINE("SERVER_NAME",$_SERVER['SERVER_NAME']);
-  //} else {
-  //  DEFINE("SERVER_NAME","localhost");
-  //}
-  DEFINE("SERVER_NAME","dropbox.joegatt.org");
+  if(isset($_SERVER['SERVER_NAME'])) {
 
-  if (SERVER_NAME == 'joegatt.net' || SERVER_NAME == 'v2.joegatt.net') {
-    DEFINE("DATABASE","joegatt-net-production-v2");
-  } else if (preg_match('/joegatt-net/',SERVER_NAME)) {
-    DEFINE("DATABASE","live");
-  } else if (SERVER_NAME == 'test.joegatt.org') {
-    DEFINE("DATABASE","joegatt-net-test");
-  } else if (SERVER_NAME == 'preview.joegatt.org') {
-    DEFINE("DATABASE","joegatt-net-preview");
+    DEFINE("SERVER_NAME",$_SERVER['SERVER_NAME']);
+    if (SERVER_NAME == 'joegatt.net' || SERVER_NAME == 'v2.joegatt.net' || SERVER_NAME == 'preview.joegatt.net') {
+       DEFINE("DATABASE","joegatt-net-production-v2");
+    } else if (SERVER_NAME == 'test.joegatt.org') {
+       DEFINE("DATABASE","joegatt-net-test");
+    } else {
+       DEFINE("DATABASE","joegatt-net-dev");
+    }
+
   } else {
-    DEFINE("DATABASE","joegatt-net-dev");
+
+    preg_match('@(/www/)([^/]+)(/)@i', getcwd(), $matches);
+    $current = $matches[2];
+
+    if ($current == 'production-v2') {
+       DEFINE("SERVER_NAME",'joegatt.net');
+       DEFINE("DATABASE","joegatt-net-production-v2");
+    } else if ($current == 'test') {
+       DEFINE("SERVER_NAME",'test.joegatt.org');
+       DEFINE("DATABASE","joegatt-net-test");
+    } else if ($current == 'dev') {
+       DEFINE("SERVER_NAME",'dev.joegatt.org');
+       DEFINE("DATABASE","joegatt-net-dev");
+    } else {
+       DEFINE("SERVER_NAME",'dropbox.joegatt.org');
+       DEFINE("DATABASE","joegatt-net-dev");
+    }
+  
   }
-  
-  //PUT THESE UNDER /tmp ?
-  
+
   if (preg_match('/joegatt-net/',SERVER_NAME)) {
     require_once("D://Documents/Websites/_db_passwords/joegatt-net/passwords.php");
   } else {
