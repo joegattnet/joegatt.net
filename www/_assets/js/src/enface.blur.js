6fd3b67f46462bf59731b8bf47d45f6682406a26
Joe Gatt <dev@joegatt.net>
2011-09-06 16:42:37 +0100

    Fixes reported total.

--- www/_assets/js/src/enface.blur.js
+++ www/_assets/js/src/enface.blur.js
@@ -14,7 +14,7 @@ NB.Enface.blur = function(event){
      var new_version_number = NB.versions['p'+j.data('id')].version + 1;
      //var new_version_number_display = NB.Anagram.total_version(NB.Anagram.total);
      j.data('p_text',current_text_raw);
-     var pending = "NB.Ajax.html('post','#hidden','/_etc/exe/enface/save.pl','score="+score+"&score_total="+NB.Anagram.snap+"&username='+NB.User.name+'&user_level='+NB.User.level+'&u='+NB.User.id+'&b="+NB.book.id+"&p="+p+"&p_id="+e.id+"&version="+(new_version_number)+"&text="+current_text+"',false);NB.Nav.track(2,'Enface', 'Saved', '+"+score+"', p);NB.Versions.display("+new_version_number+");NB.Enface.reset($('#"+e.id+"'),false);";
+     var pending = "NB.Ajax.html('post','#hidden','/_etc/exe/enface/save.pl','score="+score+"&score_total="+NB.Anagram.total+"&username='+NB.User.name+'&user_level='+NB.User.level+'&u='+NB.User.id+'&b="+NB.book.id+"&p="+p+"&p_id="+e.id+"&version="+(new_version_number)+"&text="+current_text+"',false);NB.Nav.track(2,'Enface', 'Saved', '+"+score+"', p);NB.Versions.display("+new_version_number+");NB.Enface.reset($('#"+e.id+"'),false);";
      if (NB.User.id==0){
 		 	 NB.User.pending.add(pending,10);
        NB.User.popup('save your changes');
