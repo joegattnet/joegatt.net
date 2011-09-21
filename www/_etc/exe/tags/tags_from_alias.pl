b3eb1997bc9d86679117dd14ab0ed08422c6b7db
Joe Gatt <dev@joegatt.net>
2011-09-21 14:09:18 +0100

    New script gets tags associated with content.

---
+++ www/_etc/exe/tags/tags_from_alias.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -T
+
+require '../basics.pl';
+
+formRead("get");
+
+$dbh = connectDB();
+
+my $sthTags = $dbh->prepare(qq{
+  SELECT tags.name_simple
+  FROM tags, notes, _lookup
+  WHERE textonly =  ?
+  AND NOT tags.name_simple LIKE '\\_%'
+  AND _lookup.type =0
+  AND _lookup.check1 = notes.e_guid
+  AND _lookup.check2 = tags.e_guid
+  ORDER BY tags.name_simple
+});
+$sthTags->execute($exclude_text);
+$tags = join(",", $sthTags->fetchrow_array(), $tags);
+
+$sthTags->finish();
+$dbh->disconnect();
+
+$output = qq~
+  <!--#set var="tags" value="$tags" -->
+~;
+
+# ******************************************************************************
+
+cache_output("../../cache/tags--tags_from_alias-$ENV{\"QUERY_STRING\"}.shtml",$output);
