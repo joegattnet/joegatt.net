23c19c88d043103ba19af5585fdfab55ccd896f7
Joe Gatt <dev@joegatt.net>
2011-11-05 22:51:27 +0000

    Required for previous commit.

--- www/_etc/exe/basics_pantography.pl
+++ www/_etc/exe/basics_pantography.pl
@@ -156,6 +156,53 @@ sub twitterErrors {
 
 # ******************************************************************************
 
+sub pantographyTimeline {
+  $dbh = connectDBpantography();
+  
+  #Get latest self-generated text
+  my $sth = $dbh->prepare(qq{
+    SELECT text, 
+    DATE_FORMAT(date_created,'%Y-%m-%dT%H:%i:%sZ'), 
+    DATE_FORMAT(date_created,'%e %b %Y at %H:%i UTC'), 
+    text_id, 
+    twitter_username, 
+    twitter_realname 
+    FROM text, 
+    contributors 
+    WHERE contributor_id = contributors.Id 
+    ORDER BY date_created DESC 
+    LIMIT 5
+  });
+  $sth->execute();
+  my $statuses = $sth->fetchall_arrayref();
+  
+  $sth->finish();
+  $dbh->disconnect();
+  
+  use Template;
+  
+  my $TTconfig = {
+      ENCODING => 'utf8',
+      INCLUDE_PATH => '../../templates',
+      POST_CHOMP   => 1
+  };
+  
+  my $TTtemplate = Template->new($TTconfig);
+  
+  my $TTvars = {
+    pantographyUsername => "$pantographyUsername",
+    statuses => $statuses
+  };
+  
+  my $TTinput = "pantography.timeline.tmpl";
+  
+  $TTtemplate->process($TTinput, $TTvars, \$output) || die $TTtemplate->error();
+  
+  cache_output("../../cache/pantography--timeline-.html", $output);
+}
+
+# ******************************************************************************
+
 sub sanitiseTextPantography {
   use utf8;
   use Text::Unidecode;
@@ -172,5 +219,6 @@ sub sanitiseTextPantography {
   $text =~ s/[^$pantographyAlphabet]//g;
   return $text;    
 }
+
 
 1;
