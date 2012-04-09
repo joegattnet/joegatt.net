153b1b1919e37e62f36e2aa5381983c403a81837
Joe Gatt <joe@joegatt.net>
2012-04-09 14:25:58 +0100

    Now using Less css.
    Added less css using less.inc.php and less.js for debugging, styling
    remains mostly the same.

--- www/_assets/js/src/nav.hash.js
+++ www/_assets/js/src/nav.hash.js
@@ -20,6 +20,27 @@ NB.Nav.hash = function () {
   
 }
 
-History.Adapter.bind(window, 'statechange', NB.Nav.hash);
-
 */
+
+NB.Nav.hash = function () {
+  var url;
+  if ( History.enabled ) {
+      url = NB.Url.path(location.href);
+  } else {
+    if (location.hash === '' || location.hash === '#') {
+      url = NB.Url.path(location.href.replace('#', ''));
+    } else {
+      url = location.hash.replace('#', '');
+    }
+  }
+  if (decodeURIComponent(url) !== decodeURIComponent(NB.crumb.lastloaded)) {
+    NB.Nav.track(1, 'Hash change detected.', url, NB.crumb.lastloaded);
+    NB.Nav.fetch(url);
+  }
+  //  url = NB.Url.path(location.href);
+  //  NB.Nav.fetch(url);
+  
+}
+
+
+History.Adapter.bind(window, 'statechange', NB.Nav.hash);
